6f2920610ad55952fce99c46b2ff37f7282943f6
Brandon Rockhold <brandon.rockhold@gmail.com>
2025-08-25 09:46:47 -0500

    Introducing a dark theme and implementing better responsiveness for mobile

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 text-center text-sm text-gray-500 py-4">
+    <footer className="bg-gray-900 text-gray-500 text-sm text-center py-4 border-t border-gray-800 font-mono">
       © {new Date().getFullYear()} Brandon Rockhold
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+      },
+    },
   },
   plugins: [],
 };
